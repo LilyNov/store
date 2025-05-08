6d221855b28d9b02205b82f2fb8b6ea24a4db177
Lily <lily@Lilys-Laptop.local>
2025-05-08 09:21:16 +0100

    header fix

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,6 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./module-toggle";
 import Menu from "./menu";
 
 const Header = () => {
@@ -23,19 +20,6 @@ const Header = () => {
               {APP_NAME}
             </span>
           </Link>
-        </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
         </div>
         <Menu />
       </div>
